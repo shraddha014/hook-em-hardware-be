f56e26bb5213aa93e9f920f44df248a5fc86b9d8
Alison Quan <thuqa2@fpt.com.vn>
2024-08-07 11:59:31 -0500

    Append project id  to user new project

--- src/app/create_projects.py
+++ src/app/create_projects.py
@@ -1,4 +1,5 @@
 from flask import jsonify, current_app
+from .project_list import set_project_list_user
 
 def create_project(name, user_name, description, project_id):
     db = current_app.db
@@ -10,16 +11,19 @@ def create_project(name, user_name, description, project_id):
     if not user_doc:
         return {'message': 'Error: User not found'}, 404
     else:
-        user_id = user_doc['_id']  # Retrieve the user_id from MongoDB user_collection
+        user_id = user_doc['user_name'] 
 
     if project_collection.find_one({"project_id": project_id}):
         # Check if a project with the same project_id already exists
         return {'message': 'Error: Project with this ID already exists'}, 409 
     else:
         # If no existing project is found, proceed to create a new one
-        new_project = {"name": name, "description": description, "project_id": project_id, "user_id": user_id}
+        new_project = {"name": name, "description": description, "project_id": project_id}
         result = project_collection.insert_one(new_project)
         if result.acknowledged:
+            #add project_id into the user
+            set_project_list_user({'username': user_name, 'project_id': project_id})
             return {'message': 'Project created successfully!', 'project_id': str(result.inserted_id)}, 201
         else:
             return {'message': 'Error creating project'}, 500 
+    
